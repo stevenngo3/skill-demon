508ea6ba9906f705e5b00c1e70fe0f3500604e87
stevenngo3 <s1ngo@ucsd.edu>
2022-05-04 23:56:20 -0700

    Update SkillDemooTest.java

--- SkillDemooTest.java
+++ SkillDemooTest.java
@@ -4,6 +4,6 @@ import org.junit.*;
 public class SkillDemooTest {
     @Test
     public void addition() {
-        assertEquals(2, SkillDemoo.add(1,3));
+        assertEquals(2, SkillDemoo.add(1,1));
     }
 }
